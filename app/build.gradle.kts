4a3090e7ca18a42d8e49a3afb032f5426b05eac8
Yogev <yogevbokobza12@gmail.com>
2026-09-21 21:42:54 +0300

    Add <queries> for Android 11+ package visibility; bump to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.yogev.youtubeupdater"
         minSdk = 26
         targetSdk = 31
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         vectorDrawables { useSupportLibrary = true }
     }
 
